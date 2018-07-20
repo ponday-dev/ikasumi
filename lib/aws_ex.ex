7a26450ceda3457551d83886370313b202be2f63
ponday-dev <ponday.dev+github@gmail.com>
2018-07-20 16:15:56 +0900

    [fix] remove `IO.puts`

--- lib/aws_ex.ex
+++ lib/aws_ex.ex
@@ -5,7 +5,6 @@ defmodule AWS do
     client = %{client | service: service}
     host = get_host(client, service)
     url = get_url(client, host, url)
-    IO.puts(url)
     headers = Enum.concat([{"Host", host}], headers)
     {:ok, payload} = if input != nil, do: Poison.encode(input), else: ""
 
