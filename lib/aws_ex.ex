e6e8e48f20f3ac8c90b1e154a4bcacca7d1dbdd9
ponday-dev <ponday.dev+github@gmail.com>
2018-07-24 11:48:13 +0900

    [update] add `AWS.get_credentials`

--- lib/aws_ex.ex
+++ lib/aws_ex.ex
@@ -1,5 +1,5 @@
 defmodule AWS do
-  alias AWS.{Client, Request}
+  alias AWS.{Client, Request, Cognito}
   alias AWS.Request.Signer
 
   def request(%Client{} = client, %Request{} = request, options \\ []) do
@@ -10,5 +10,15 @@ defmodule AWS do
     url = Request.url(client, request)
     url = if length(request.query_params) >= 1, do: url <> "?" <> URI.encode_query(request.query_params), else: url
     HTTPoison.request(request.method, url, request.payload, request.headers, options)
+  end
+
+  def get_credentials(client, identity_id) do
+    with {:ok, %{"Credentials" => credentials}, _}<- Cognito.get_credentials_for_identity(identity_id) do
+      %{client |
+        access_key: Map.get(credentials, "AccessKeyId"),
+        secret_access_key: Map.get(credentials, "SecretKey"),
+        credentials: credentials
+      }
+    end
   end
 end
