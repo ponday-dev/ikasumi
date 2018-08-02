850083becfd580ffe0bce1192322cc16249b5498
ponday-dev <ponday.dev+github@gmail.com>
2018-08-02 10:25:52 +0900

    [delete] unused alias

--- lib/aws/request/request.ex
+++ lib/aws/request/request.ex
@@ -21,7 +21,7 @@ defmodule AWS.Request do
   ]
 
   def url(%Client{} = client, %Request{} = request) do
-    endpoint = "#{client.proto}://#{request.host}:#{client.port}#{request.path}"
+    "#{client.proto}://#{request.host}:#{client.port}#{request.path}"
   end
 
   def encode!(%Request{} = request, :payload) do
