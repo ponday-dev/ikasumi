df184d1cbb6f700d047ddf711458faba2d33da17
ponday-dev <ponday.dev+github@gmail.com>
2018-09-07 14:07:02 +0900

    [update] cognito: add function `get_id`

--- lib/ikasumi/cognito/cognito.ex
+++ lib/ikasumi/cognito/cognito.ex
@@ -2,15 +2,33 @@ defmodule Ikasumi.Cognito do
   def get_credentials_for_identity(client, identity_id) do
     request(client, "/", "GetCredentialsForIdentity", %{ IdentityId: identity_id }) |> send_request(client)
   end
-  def get_credentials_for_identity(client, identity_id, id_token) do
+  def get_credentials_for_identity(client, id_provider, identity_id, id_token) do
     payload = %{
-      IdentityId: identity_id,
-      Logins: %{
-        "cognito-identity.#{client.region}.#{client.endpoint}/#{client.user_pool_id}" => id_token
+      "IdentityId" => identity_id,
+      "Logins" => %{
+        provider(client, id_provider) => id_token
       }
     }
     request(client, "/", "GetCredentialsForIdentity", payload) |> send_request(client)
   end
+
+  def get_id(client, id_provider, token) do
+    payload = %{
+      "IdentityPoolId" => client.identity_pool_id,
+      "Logins" => %{
+        provider(client, id_provider) => token
+      }
+    }
+    |> (fn body -> if %{account_id: ""} = client, do: body, else: %{body | "AccountId" => client.account_id} end).()
+
+    request(client, "/", "GetId", payload) |> send_request(client)
+  end
+
+  defp provider(_, :google), do: "accounts.google.com"
+  defp provider(client, :cognito_user_pool), do: "cognito-idp.#{client.region}.amazonaws.com/#{client.user_pool_id}"
+  defp provider(_, :facebook), do: "graph.facebook.com"
+  defp provider(_, :twitter), do: "api.twitter.com"
+  defp provider(_, name), do: name
 
   defp request(client, path, action, payload) do
     %Ikasumi.Request{
